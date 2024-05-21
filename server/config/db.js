c196a61045721bb386c9f5595114041ce426b67a
markointihar <47603551+markointihar@users.noreply.github.com>
2024-05-21 11:18:03 +0200

    povezava z bazo v oblaku

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql');
-
+const mysql = require('mysql2');
+require('dotenv').config();
 // Konfiguracija povezave z MySQL podatkovno bazo
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'my_database'
-});
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+  });
+  
 
 // Povezovanje z bazo
 connection.connect((err) => {

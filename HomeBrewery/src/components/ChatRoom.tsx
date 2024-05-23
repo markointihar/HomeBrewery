38b21aad6bb10b068d09b0645d7d67bcb3f569da
markointihar <47603551+markointihar@users.noreply.github.com>
2024-05-23 11:34:15 +0200

    izboljšan in polepšan chatroom

--- HomeBrewery/src/components/ChatRoom.tsx
+++ HomeBrewery/src/components/ChatRoom.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import '../css/chatroom.css'
 
 import io from 'socket.io-client';
 
@@ -11,40 +12,78 @@ const ChatRoom = () => {
     const { roomId } = useParams();
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState<string[]>([]);
-
+    const [username, setUsername] = useState('');
+    const [isConnected, setIsConnected] = useState(false);
     useEffect(() => {
+        if (username && !isConnected) {
+          socket.emit('new-user', username);
+        }
+    
         socket.emit('joinRoom', roomId);
-
-        socket.on('message', (message: string) => {
-            setMessages((prevMessages) => [...prevMessages, message]);
+    
+        socket.on('chat-message', (data: { message: string; name: string }) => {
+          setMessages((prevMessages) => [...prevMessages, `${data.name}: ${data.message}`]);
         });
-
+    
+    
         return () => {
-            socket.off('message');
+          socket.off('chat-message');
+          socket.off('user-connected');
+          socket.off('user-disconnected');
         };
-    }, [roomId]);
-
-    const sendMessage = () => {
-        socket.emit('message', { room: roomId, message });
-        setMessage('');
-    };
-
-    return (
-        <div>
-            <div>
-                {messages.map((msg, index) => (
-                    <div key={index}>{msg}</div>
-                ))}
+      }, [roomId, username, isConnected]);
+    
+      const sendMessage = () => {
+          socket.emit('new-user', username);
+          
+    
+        if (message.trim()) {
+          socket.emit('send-chat-message', message);
+          setMessages((prevMessages) => [...prevMessages, `You: ${message}`]);
+          setMessage('');
+        }
+        setIsConnected(true);
+      };
+    
+      return (
+        <>
+            <div className='chat-container'>
+            <h1>Chat Room</h1>
+            <p>Napiši username in počakaj da se ti pridruži strokovnjak</p>
+            <div className='sporocila'>
+                {messages.map((msg, index) => {
+                    const className = msg.startsWith('You:') ? 'your-message' : 'other-message';
+                    return (
+                        <div className={className} key={index}>
+                        {msg}
+                        </div>
+                    );
+                })}
             </div>
-            <input 
-                type="text" 
-                value={message} 
-                onChange={(e) => setMessage(e.target.value)} 
-                placeholder="Enter message"
-            />
-            <button onClick={sendMessage}>Send</button>
-        </div>
-    );
-};
+            <div className='forma'>
+                {!isConnected && (      
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Username"
+                    />
+
+                )}
+                    <input
+                        type="text"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        placeholder="Napiši sporočilo..."
+                    />
+                    <button onClick={sendMessage}>Send</button>
+
+            </div>
+            
+            </div>
+        </>
+        
+      );
+    };
 
 export default ChatRoom;
--- HomeBrewery/src/components/Navbar.tsx
+++ HomeBrewery/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import beer from '../assets/beer.svg';
 import pfp from '../assets/profile.svg'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Navbar(){
     const [roomLink, setRoomLink] = useState('');
@@ -17,14 +17,22 @@ export default function Navbar(){
     
     return (
         <div className="navbar">
-            <img src={beer}/>
-            <div className='nav-items'>
+            <img src={beer} alt='beer'/>
+            
+
+            
+            <div className='nav-items' >
                 <div className="item" onClick={createRoom}>
                     Pomoč
                 </div>
+                
                 <div className="item">
-                    prvi
+                    <Link to={'/'} >
+                        Home
+                    </Link>
                 </div>
+                
+                
                 <div className="item">
                     prvi
                 </div>
